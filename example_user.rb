2b2f13e16268f2cae21eec81fa66c3309dd879b3
jamesplumbtree <nickcollins79@yahoo.com>
2012-05-15 15:18:44 -0400

    chapter 5

---
+++ example_user.rb
@@ -0,0 +1,16 @@
+class User
+  attr_accessor :name, :email
+
+  def initialize(attributes = {})
+    @name  = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
+  
+  def shuffle_name
+    "#{@name}".split('').shuffle 
+  end
+end
